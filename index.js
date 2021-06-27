c0c6c641744ab9c0eaf1b5ef7f8deb9d9c63abb1
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-06-27 13:57:09 +0200

    dynamically working spotify

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
-async function loadSongs(artist){
 
-    let response = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist, {
+
+let songsData = []
+function loadSongs(artist){
+
+   fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist, {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "652a249567msha16735a3902749dp1b0cdajsnf44568c1e811",
+            "x-rapidapi-key": "60669e97f7mshf88d946ea97bc61p1f9f34jsnbfbce3cb9a39",
             "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
         }
     })
-    let songsResponse = await response.json()
-    return songsResponse.data
+    .then((response) => response.json())
+    .then((songs)=>{
+      console.log('here',songs)
+      for(let songList in songs.data){
+        songsData[songList] = {
+          name:songs.data[songList].artist.name,
+          title:songs.data[songList].artist.title,
+          image:songs.data[songList].album.cover_medium,
+        }
+      }
+      
+      console.log('small obj',songsData);
+
+      renderSongs()
+    })
+    .catch((err)=>{
+      console.error(err)
+    })
+
+
+   
     
+    
+    
+   
+  
+    
+}
+
+
+
+ function renderSongs(){
+   
+    let songsDiv = document.getElementById('bring-here')
+    songsData.forEach(song => songsDiv.innerHTML += 
+        `<div class="col pl-0">
+                  <div class="card h-100">
+                    <div class="position-relative">
+                      <img
+                        src="${song.image}"
+                        class="card-img-top img-fluid rounded py-3 px-2"
+                        alt="..."
+                      />
+
+                      <div class="play"></div>
+                    </div>
+
+                    <div class="card-body px-2 pt-0" id="song-here">
+                      <h5 class="card-title mb-1 text-white">
+                       ${song.name}
+                      </h5>
+                      <p class="card-text text-white-50">${song.title}</p>
+                    </div>
+                  </div>
+                </div>`)
+                
+}
+
+
+window.onload = () =>{
+
+  loadSongs('drake')
+  
+
 }
